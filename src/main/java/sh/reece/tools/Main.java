24d298468a6cf22e0fb402c1c850ca60e2361304
Reece Williams <reecepbcups@gmail.com>
2026-03-31 20:00:47 -0500

    sets instead of lists

--- src/main/java/sh/reece/tools/Main.java
+++ src/main/java/sh/reece/tools/Main.java
@@ -154,12 +154,9 @@ public class Main extends JavaPlugin implements Listener {
 	}
 
 	public static String replaceVariable(String line) {
-		if (SERVER_VARIABLE_KEYS.stream().anyMatch(line::contains)) {
-			for (String key : SERVER_VARIABLE_KEYS) {
-				if(line.contains("%"+key+"%")) {
-					line = line.replace("%"+key+"%", SERVER_VARIABLES.get(key));
-				}
-			}
+		if (!line.contains("%")) return line;
+		for (var entry : SERVER_VARIABLES.entrySet()) {
+			line = line.replace("%" + entry.getKey() + "%", entry.getValue());
 		}
 		return line;
 	}
