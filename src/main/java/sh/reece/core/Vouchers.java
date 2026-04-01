ba62948e0e43f2ef2c7fae25ad249dd5594ccc2d
Reece Williams <reecepbcups@gmail.com>
2026-03-31 19:49:26 -0500

    simplify arguments

--- src/main/java/sh/reece/core/Vouchers.java
+++ src/main/java/sh/reece/core/Vouchers.java
@@ -178,7 +178,7 @@ public class Vouchers implements Listener, CommandExecutor, TabCompleter {
         return item;
     }
 
-	@EventHandler(ignoreCancelled = true)
+	@EventHandler
 	public void playerClickedOnVoucher(PlayerInteractEvent e) {
 		// click of voucher	if in keys
 
@@ -335,7 +335,7 @@ public class Vouchers implements Listener, CommandExecutor, TabCompleter {
 	}
 
 
-	@EventHandler(ignoreCancelled = true) // gives itemstack on click
+	@EventHandler // gives itemstack on click
 	public void onInventoryClick(InventoryClickEvent event) {
 		ItemStack clicked = event.getCurrentItem();
 
